d99994f9c918866ab266739fb568c7b9179bff5c
Martiruhay <marti-nini@hotmail.com>
2018-04-03 19:17:33 +0200

    Another failed attempt

--- Assets/_Scripts/PortalCamera.cs
+++ Assets/_Scripts/PortalCamera.cs
@@ -6,7 +6,7 @@ public class PortalCamera : MonoBehaviour
 {
     Transform player;
     Transform playerCamera;
-    Vector3 offset;
+    //Vector3 offset;
     public Transform portal1, portal2;
 
     void Start ()
@@ -18,16 +18,32 @@ public class PortalCamera : MonoBehaviour
         }
         playerCamera = player.GetChild(0);
 
-        offset = player.position - transform.position;
+        //offset = player.position - transform.position;
     }
 	
 	void Update ()
     {
-        Vector3 newPos = portal1.InverseTransformPoint(player.position);
-        transform.localPosition = new Vector3(-newPos.x, transform.localPosition.y, -newPos.z);
+        Quaternion Rot180 = Quaternion.Euler(0, 180.0f, 0);
+        Vector3 LocalPos = portal1.InverseTransformPoint(playerCamera.position);
+        Vector3 RotatedPos = Rot180 * LocalPos;
+        transform.position = portal2.TransformPoint(RotatedPos);
+        transform.rotation = portal2.rotation * Rot180 * Quaternion.Inverse(portal1.transform.rotation) * transform.rotation;
 
-        float angle = Vector3.SignedAngle(-portal1.transform.forward, player.forward, Vector3.up)/2;
-        transform.localRotation = Quaternion.Euler(0, angle, 0);
+        //Vector3 newPos = portal1.InverseTransformPoint(playerCamera.position);
+        //transform.localPosition = new Vector3(-newPos.x, newPos.y, -newPos.z);
+        //
+        //Vector3 playerToPortal = portal1.position - playerCamera.position;
+        //float angleTest = Vector3.SignedAngle(-portal1.transform.forward.normalized, playerToPortal.normalized, Vector3.up);
+        //
+        //Debug.DrawLine(playerCamera.position, portal1.position);
+        //Debug.DrawRay(portal1.position, portal1.forward * 100, Color.blue);
+        //
+        //float angle = Vector3.SignedAngle(-portal1.transform.forward, player.forward, Vector3.up);
+        //print(angleTest);
+        //
+        //transform.localRotation = Quaternion.Euler(transform.localEulerAngles.x, angleTest, transform.localEulerAngles.z);
+        //transform.forward = playerToPortal.normalized;
+        //print(transform.localEulerAngles);
 
         //print(angle);
     }
