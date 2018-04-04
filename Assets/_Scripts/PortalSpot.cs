164b7761ef5967a74bd2624d710b5e587f60dd73
Martiruhay <marti-nini@hotmail.com>
2018-04-04 02:50:07 +0200

    Refactor: Working and modular portals

--- Assets/_Scripts/PortalSpot.cs
+++ Assets/_Scripts/PortalSpot.cs
@@ -4,25 +4,59 @@ using UnityEngine;
 
 public class PortalSpot : MonoBehaviour
 {
-    public int id;
-    public PortalSpot nextSpot;
-    public Transform spawnPoint;
+    // Static
+    Transform playerCamera;
 
-	void Start ()
+    // Public
+    public PortalSpot destination;
+    public Transform portalCamera;
+    public MeshRenderer portalQuad;
+
+    // Private
+    RenderTexture portalTexture;
+
+    // Events
+    void Awake()
     {
-		
-	}
+        portalTexture = new RenderTexture(Screen.width, Screen.height, 16);
+        portalTexture.Create();
+        portalQuad.material.mainTexture = portalTexture;
+    }
+
+    void Start ()
+    {
+        // Get player's transform
+        if (playerCamera == null)
+        {
+            playerCamera = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0);
+        }
+
+        // Set portalCamera's target texture
+        portalCamera.GetComponent<Camera>().targetTexture = portalTexture;
+        //portalCamera.parent = null;
+    }
 	
 	void Update ()
     {
-		
-	}
+        if (destination != null)
+            handlePortalCamera();
+    }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            PortalsController.instance.Teleport(id);
+            // Check for teleport position
         }
+    }
+
+    // Private functions
+    void handlePortalCamera()
+    {
+        Quaternion Rot180 = Quaternion.Euler(0, 180.0f, 0);
+        Vector3 LocalPos = transform.InverseTransformPoint(playerCamera.position);
+        Vector3 RotatedPos = Rot180 * LocalPos;
+        portalCamera.position = destination.transform.TransformPoint(RotatedPos);
+        portalCamera.rotation = destination.transform.rotation * Rot180 * Quaternion.Inverse(transform.rotation) * playerCamera.rotation;
     }
 }
